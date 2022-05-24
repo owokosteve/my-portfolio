2fcd373b523092910eb3dbc6627a8a4c6e071500
= <=>
2022-05-24 21:23:32 +0300

    added blog data object

---
+++ blog-data.ts
@@ -0,0 +1,13 @@
+const blog = [
+    {
+        "id": "1",
+        "title": "",
+        "category": "Technology",
+        "img-url": "http:www.xyz.com",
+        "gravatar-url": "",
+        "author": "Stephen Otieno",
+        "date": new Date(),
+        "duration": "",
+        "content": ""
+    }
+]
