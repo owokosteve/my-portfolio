598a7016acb34448d7ad005b1b78e2e46f201d55
= <=>
2022-05-24 20:46:22 +0300

    Refoctored the homepage

--- steve-app/src/app/app.module.ts
+++ steve-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { NavComponent } from './home/nav/nav.component';
+import { FooterComponent } from './home/footer.component';
+import { HeroComponent } from './home/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BlogComponent,
+    NavComponent,
+    FooterComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
